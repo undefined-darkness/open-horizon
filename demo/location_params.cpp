90ed738e2b2c8b6433bd301431d7aa1d30225467
Undefined Darkness <undefined_darkness@outlook.com>
2015-03-25 03:32:08 +0300

    location params

--- demo/location_params.cpp
+++ demo/location_params.cpp
@@ -6,6 +6,8 @@
 #include "resources/resources.h"
 #include "memory/memory_reader.h"
 #include "memory/tmp_buffer.h"
+#include "math/quaternion.h"
+#include "math/constants.h"
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
@@ -25,7 +27,40 @@ bool location_params::load(const char *file_name)
 
     nya_memory::tmp_buffer_scoped fi_data(file_data->get_size());
     file_data->read_all(fi_data.get_data());
-    nya_memory::memory_reader reader(fi_data.get_data(), file_data->get_size());
+
+    class memory_reader: public nya_memory::memory_reader
+    {
+    public:
+        location_params::color3 read_color3()
+        {
+            location_params::color3 c;
+            c.z = read<float>();
+            c.y = read<float>();
+            c.x = read<float>();
+            return c / 255.0;
+        }
+
+        location_params::color4 read_color4()
+        {
+            location_params::color4 c;
+            c.w = read<float>();
+            c.z = read<float>();
+            c.y = read<float>();
+            c.x = read<float>();
+            return c / 255.0;
+        }
+
+        location_params::vec3 read_dir_py()
+        {
+            float pitch = read<float>() * nya_math::constants::pi / 180.0f;
+            float yaw = read<float>() * nya_math::constants::pi / 180.0f;
+
+            return nya_math::quat(pitch,yaw,0.0f).rotate(nya_math::vec3(0.0,1.0,0.0));
+        }
+        
+        memory_reader(const void *data, size_t size): nya_memory::memory_reader(data, size) {}
+
+    } reader(fi_data.get_data(), file_data->get_size());
 
     clipping_plane.ground_zfar = reader.read<float>();
     clipping_plane.ground_znear = reader.read<float>();
@@ -42,7 +77,26 @@ bool location_params::load(const char *file_name)
     reader.skip(11*4); //debug
     reader.skip(3*4); //detail
     reader.skip(8*4); //map parts
-    reader.skip(129*4); //sky
+
+    reader.skip(18*4); //sky weather
+
+    sky.fog_density = reader.read<float>();
+    sky.fog_height = reader.read<float>();
+    sky.fog_height_density = reader.read<float>();
+    sky.fog_height_fade_density = reader.read<float>();
+    sky.fog_height_fresnel = reader.read<float>();
+
+    reader.skip(36*4); //sky high
+    reader.skip(36*4); //sky low
+    reader.skip(25*4); //sky mapspecular
+
+    sky.moon_dir = reader.read_dir_py();
+    sky.moon_color = reader.read_color3();
+    sky.moon_size = reader.read<float>();
+
+    sky.sun_dir = reader.read_dir_py();
+    sky.sun_size = reader.read<float>();
+
     reader.skip(93*4+5*2); //tree
 
     assert(!reader.get_remained());
--- demo/location_params.h
+++ demo/location_params.h
@@ -24,6 +24,9 @@ struct location_params
 
     typedef value<float> fvalue;
     typedef value<unsigned int> uvalue;
+    typedef nya_math::vec3 color3;
+    typedef nya_math::vec4 color4;
+    typedef nya_math::vec3 vec3;
 
     struct
     {
@@ -33,6 +36,94 @@ struct location_params
         fvalue ground_zfar;
 
     } clipping_plane;
+
+    struct skysphere
+    {
+        color3 ambient;
+        fvalue lens_brightness;
+
+        color3 light2_color;
+        vec3 light2_dir;
+        fvalue light2_power;
+
+        color3 light3_color;
+        vec3 light3_dir;
+        fvalue light3_power;
+
+        fvalue player_shadow_brightness;
+        color3 player_shadow_color;
+
+        fvalue skysphere_intensity;
+        color3 specular_color;
+        fvalue specular_power;
+
+        color3 sun_color;
+        fvalue sun_power;
+        color3 sun_rgb;
+
+        color3 sun_flare_rgb;
+        fvalue sun_flare_size;
+    };
+
+    struct
+    {
+        struct
+        {
+            fvalue gravity_speed;
+            vec3 gravity_dir;
+
+            color4 air_color;
+            fvalue air_ratio;
+
+            color4 rain_color;
+            fvalue rain_ratio;
+
+            color4 snow_color;
+            fvalue snow_ratio;
+
+        } weather;
+
+        fvalue fog_density;
+        fvalue fog_height;
+        fvalue fog_height_density;
+        fvalue fog_height_fade_density;
+        fvalue fog_height_fresnel;
+
+        skysphere low;
+        skysphere high;
+
+        vec3 moon_dir;
+        color3 moon_color;
+        fvalue moon_size;
+
+        vec3 sun_dir;
+        fvalue sun_size;
+
+        struct
+        {
+            color3 mesh_color;
+            fvalue mesh_contrast;
+            fvalue mesh_fog_power;
+            fvalue mesh_fog_fresnel;
+            fvalue mesh_fog_fresnel_max;
+            fvalue mesh_power;
+            fvalue mesh_scale;
+
+            color3 obj_ambient;
+            color3 obj_sun_color;
+
+            color3 parts_color;
+            fvalue parts_contrast;
+            fvalue parts_fog_power;
+            fvalue parts_fresnel;
+            fvalue parts_fresnel_max;
+            fvalue parts_power;
+            fvalue parts_reflection_power;
+            fvalue parts_scale;
+
+        } mapspecular;
+
+    } sky;
 
     //ToDo
 
