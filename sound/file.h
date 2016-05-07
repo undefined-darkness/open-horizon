34489212ab8a432e83bb071f5b9d4e9c120dbde5
Undefined Darkness <undefined_darkness@outlook.com>
2016-05-07 21:01:36 +0300

    sound

--- sound/file.h
+++ sound/file.h
@@ -4,13 +4,15 @@
 
 #pragma once
 
-#include "file.h"
-#include "clHCA.h"
 #include <vector>
 #include <memory>
 
 namespace sound
 {
+//------------------------------------------------------------
+
+class hsa_bit_stream;
+
 //------------------------------------------------------------
 
 class file
@@ -31,13 +33,36 @@ private:
     struct hca_data
     {
         uint16_t version = 0;
-        uint8_t channels = 0;
         uint32_t samples_per_second = 0;
         uint32_t block_count = 0;
         uint16_t block_size = 0;
-        float volume = 1.0f;
         uint32_t loop_start = 0, loop_end = 0;
         std::vector<char> raw_data;
+
+        void decode(hsa_bit_stream &data);
+
+        struct channel
+        {
+            float block[0x80];
+            float base[0x80];
+            char value[0x80];
+            char scale[0x80];
+            char value2[8];
+            int count;
+
+            float sample1[0x80];
+            float sample2[0x80];
+            float sample3[0x80];
+            float samples[8][0x80];
+
+            void decode1(hsa_bit_stream &data, int t);
+            void decode2(hsa_bit_stream &data);
+            void decode5(int index);
+
+            channel() { memset(this, 0, sizeof(channel)); }
+        };
+
+        std::vector<channel> channels;
     };
 
     std::shared_ptr<hca_data> m_hca_data;
